a3756080a7b03ec41e28376efc7f8237bcd5a3e4
crypticani <aniket.kumar@hotmail.com>
2022-03-15 10:41:52 +0530

    analytical skills materials

---
+++ Semester2/Java/JavaPrograms/Calculator.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public static int addition(int x, int y){
+        return x+y;
+    }
+    public static int division(int x, int y){
+        return x/y;
+    }
+    public static int subtraction(int x, int y){
+        return x-y;
+    }
+    public static void main(String[] args) {
+        Scanner sc  = new Scanner(System.in);
+        System.out.println("Enter the numbers: ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int add = addition(a, b);
+        int div = division(a, b);
+        int sub = subtraction(a, b);
+        System.out.println("Addition: "+add);
+        System.out.println("Division: "+div);
+        System.out.println("Subtraction: "+sub);
+    }
+}
--- Semester2/Java/JavaPrograms/hello.java
+++ Semester2/Java/JavaPrograms/hello.java
@@ -1,5 +1,6 @@
 class hello{
 	public static void main(String [] args){
-		System.out.println("Hello World");
+		Integer a = 5;
+		System.out.println("Hello World. "+a);
 	}
 }
