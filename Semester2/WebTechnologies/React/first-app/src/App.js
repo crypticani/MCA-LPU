4389360f458d0fd5875c84b72cfa8c1b57acf706
crypticani <aniket.kumar@hotmail.com>
2022-03-19 10:02:50 +0530

    hitbullseye week6

---
+++ Semester2/WebTechnologies/React/first-app/src/App.js
@@ -0,0 +1,26 @@
+import logo from './knowledgewit_square.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <h1>Hello!</h1>
+          <h3>This is Aniket</h3>
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
