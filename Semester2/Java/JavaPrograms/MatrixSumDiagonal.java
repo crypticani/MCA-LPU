34b2e7fb9fb1e6b51231a2a178910296fea1ee25
crypticani <aniket.kumar@hotmail.com>
2022-03-31 14:40:43 +0530

    added new materials

--- Semester2/Java/JavaPrograms/MatrixSumDiagonal.java
+++ Semester2/Java/JavaPrograms/MatrixSumDiagonal.java
@@ -4,40 +4,40 @@ import java.util.Scanner;
 
 public class MatrixSumDiagonal {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print("Enter no of rows: ");
-        int row = sc.nextInt();
-        System.out.print("Enter no of coloumns: ");
-        int col = sc.nextInt();
-        int arr[][] = new int[row][col];
-        int sum[] = new int[2] ;
-        
-        System.out.println("Enter numbers: ");
-        for(int i=0; i<row; i++){
-            for(int j=0; j<col; j++){
-                arr[i][j] = sc.nextInt();
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("Enter no of rows: ");
+            int row = sc.nextInt();
+            System.out.print("Enter no of coloumns: ");
+            int col = sc.nextInt();
+            int arr[][] = new int[row][col];
+            int sum[] = new int[2] ;
+            
+            System.out.println("Enter numbers: ");
+            for(int i=0; i<row; i++){
+                for(int j=0; j<col; j++){
+                    arr[i][j] = sc.nextInt();
+                }
             }
+
+            // //calculating sum using two loops
+            // for(int i=0; i<row; i++){
+            //     for(int j=0; j<col; j++){
+            //         if(i==j){ //left to right diagonal
+            //             sum[0]+=arr[i][j];
+            //         }
+            //         else if(i+j == col-1){ //right to left diagonal
+            //             sum[1]+=arr[i][j];
+            //         }
+            //     }
+            // }
+
+            //calculating sum using single loop
+            for(int i=0; i<col; i++){
+                sum[0]+=arr[i][i];  //left to right diagonal
+                sum[1]+=arr[i][col-i-1];  //right to left diagonal
+            }
+
+                System.out.print(sum[1]+ " " +sum[0]);
         }
-
-        // //calculating sum using two loops
-        // for(int i=0; i<row; i++){
-        //     for(int j=0; j<col; j++){
-        //         if(i==j){ //left to right diagonal
-        //             sum[0]+=arr[i][j];
-        //         }
-        //         else if(i+j == col-1){ //right to left diagonal
-        //             sum[1]+=arr[i][j];
-        //         }
-        //     }
-        // }
-
-        //calculating sum using single loop
-        for(int i=0; i<col; i++){
-            sum[0]+=arr[i][i];  //left to right diagonal
-            sum[1]+=arr[i][col-i-1];  //right to left diagonal
-        }
-
-            System.out.print(sum[1]+ " " +sum[0]);
     }
 }
--- Semester2/Java/JavaPrograms/VoteEligibility.java
+++ Semester2/Java/JavaPrograms/VoteEligibility.java
@@ -5,16 +5,16 @@ import java.util.Scanner;
 public class VoteEligibility {
     public static void main(String args[])
     {
-        Scanner sc = new Scanner(System.in);
+        try (Scanner sc = new Scanner(System.in)) {
+            int age;
 
-        int age;
+            System.out.println("Enter your age:");
+            age=sc.nextInt();
 
-        System.out.println("Enter your age:");
-        age=sc.nextInt();
-
-        if(age>=18)
-            System.out.println("You are eligible to cast your vote");
-        else
-            System.out.println("You are not eligible to cast your vote");
+            if(age>=18)
+                System.out.println("You are eligible to cast your vote");
+            else
+                System.out.println("You are not eligible to cast your vote");
+        }
     }
 }
