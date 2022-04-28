950bd9ff2774575e000a96c894c4581b77640270
crypticani <aniket.kumar@hotmail.com>
2022-04-28 16:59:27 +0530

    react app for ca3

---
+++ Semester2/WebTechnologies/React/ca3/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Props from './components/Props';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Web Technologies CA3</h1>
+        <h2>Demonstration of Components and Props</h2>
+        <Props />
+      </header>
+    </div>
+  );
+}
+
+export default App;
